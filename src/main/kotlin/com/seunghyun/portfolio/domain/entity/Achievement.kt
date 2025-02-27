1c3869d6efdec33bfee696835bbd48e49c79c2eb
seunghyun <koreas9408@gmail.com>
2025-02-27 18:53:43 +0900

    Add Entity

--- src/main/kotlin/com/seunghyun/portfolio/domain/entity/Achievement.kt
+++ src/main/kotlin/com/seunghyun/portfolio/domain/entity/Achievement.kt
@@ -1,9 +1,16 @@
 package com.seunghyun.portfolio.domain.entity
 
 import jakarta.persistence.*
+import java.time.LocalDate
 
 @Entity
-class Achievement : BaseEntity() {
+class Achievement(
+    title: String,
+    description: String,
+    achievedDate: LocalDate?,
+    host: String,
+    isActive: Boolean,
+) : BaseEntity() {
 
     /*
         TABLE: ID만을 다루는 테이블을 생성
@@ -14,5 +21,15 @@ class Achievement : BaseEntity() {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "achievement_id")
     var id: Long? = null
+
+    var title: String = title
+
+    var description: String = description
+
+    var achievedDate: LocalDate? = achievedDate
+
+    var host: String = host
+
+    var isActive: Boolean = isActive
 
 }
--- src/main/kotlin/com/seunghyun/portfolio/domain/entity/Skill.kt
+++ src/main/kotlin/com/seunghyun/portfolio/domain/entity/Skill.kt
@@ -1,13 +1,25 @@
 package com.seunghyun.portfolio.domain.entity
 
+import com.seunghyun.portfolio.domain.constant.SkillType
 import jakarta.persistence.*
 
 @Entity
-class Skill : BaseEntity() {
+class Skill(
+    name: String,
+    type: String,
+    isActive: Boolean,
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
 
+    var name: String = name
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "skill_type")
+    var type: SkillType = SkillType.valueOf(type)
+
+    var isActive: Boolean = isActive
 }
