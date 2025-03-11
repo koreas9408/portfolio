3fb34a1bf4fd16ad0d5338c09a9ecba536f269f8
seunghyun <koreas9408@gmail.com>
2025-03-11 22:11:01 +0900

    feat: Repository 성능 개선

--- src/main/kotlin/com/seunghyun/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/seunghyun/portfolio/domain/repository/ProjectRepository.kt
@@ -2,8 +2,13 @@ package com.seunghyun.portfolio.domain.repository
 
 import com.seunghyun.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
-    // 이대로 사용하면 성능에 굉장히 치명적이다. (개선 필요!)
+    @Query("select p from Project p left join fetch p.skills s left join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
+
+    @Query("select p from Project p left join fetch p.details where p.id = :id")
+    override fun findById(id: Long): Optional<Project>
 }
