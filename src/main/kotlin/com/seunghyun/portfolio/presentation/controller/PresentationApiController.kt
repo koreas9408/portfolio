086a81567c6420b0514645b705a19368c837f0f8
seunghyun <koreas9408@gmail.com>
2025-03-12 23:12:50 +0900

    feat: Controller 개발

--- src/main/kotlin/com/seunghyun/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/seunghyun/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,15 +1,43 @@
 package com.seunghyun.portfolio.presentation.controller
 
+import com.seunghyun.portfolio.presentation.dto.IntroductionDTO
+import com.seunghyun.portfolio.presentation.dto.LinkDTO
+import com.seunghyun.portfolio.presentation.dto.ProjectDTO
+import com.seunghyun.portfolio.presentation.dto.ResumeDTO
+import com.seunghyun.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK"
     }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
+
 }
